5bbc6283a732ad481ddb960a0394786f2764b3b0
Hangyu Feng <hfeng@financeit.io>
2021-07-02 01:59:23 -0400

    .

--- setup.ps1
+++ setup.ps1
@@ -2,11 +2,12 @@ $vscode_settings_path = "~\AppData\Roaming\Code\User\settings.json"
 $vscode_settings_url = "https://raw.githubusercontent.com/hangyu-feng/alain/master/configs/vscode/extensions.txt"
 $vscode_extensions_url = "https://raw.githubusercontent.com/hangyu-feng/alain/master/configs/vscode/extensions.txt"
 
-$programs = @('git', 'vscode', 'selenium')
+$programs = @('vscode', 'selenium')
 
 # install scoop
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 iwr -useb get.scoop.sh | iex
+scoop install git
 scoop bucket add extras
 
 # install programs using scoop
